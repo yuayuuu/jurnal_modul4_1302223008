6b54f1abc7a357047ec46d2d7e78737cb6eafae7
yuayuuu <4yulestarii@gmail.com>
2024-03-16 08:30:03 +0700

    menambahkan class KodeBuah

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+// asprak MKR
+
+public class kodebuah
+{
+    Dictionary<string, string> kodeBuah = new Dictionary<string, string>()
+    {
+        {"Apel", "A00"},
+        {"Aprikot", "B00"},
+        {"Alpukat", "C00"},
+        {"Pisang", "D00"},
+        {"Paprika", "E00"},
+        {"Blackberry", "F00"},
+        {"Ceri", "H00"},
+        {"Kelapa", "I00"},
+        {"Jagung", "J00"},
+        {"Kurma", "K00"},
+        {"Durian", "L00"},
+        {"Anggur", "M00"},
+        {"Melon", "N00"},
+        {"Semangka", "O00"},
+    };
+    public string getkodeBuah(string buah)
+    {
+        if (kodeBuah.ContainsKey(buah))
+        {
+            return kodeBuah[buah];
+        } 
+        else
+        {
+            return "Buah tidak ditemukan";
+        }
+
+    }
+}
